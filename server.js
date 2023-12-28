c62403013016b84f459d60158084e9939300574d
jatin panesiya <jatinpanesiya2003@gmail.com>
2023-12-28 12:29:14 +0530

    ....

--- server.js
+++ server.js
@@ -3,14 +3,21 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const { v4: uuidV4 } = require('uuid');
+const path = require("path");
+const cors = require("cors");
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views')); // Update the views path accordingly
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views")); // Update the views path accordingly
 
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.get('/', (req, res) => {
-    res.redirect(`/${uuidV4()}`);
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidV4()}`);
 });
 
 app.get("/healthcheck", (req, res) => {
@@ -18,21 +25,25 @@ app.get("/healthcheck", (req, res) => {
   res.send({ success: true });
 });
 
-app.get('/:room', (req, res) => {
-    res.render('room', { roomId: req.params.room });
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
 });
 
-io.on('connection', (socket) => {
-    socket.on('join-room', (roomId, userId) => {
-        socket.join(roomId);
-        socket.broadcast.emit('user-connected', userId);
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.broadcast.emit("user-connected", userId);
 
-        socket.on('disconnect', () => {
-            socket.broadcast.emit('user-disconnected', userId);
-        });
+    socket.on("disconnect", () => {
+      socket.broadcast.emit("user-disconnected", userId);
     });
+  });
 });
 
-server.listen(3000, () => {
-    console.log('Server running on port 3000');
+server.listen(3005, () => {
+  try {
+    console.log("Server running on port 3005");
+  } catch (err) {
+    console.log("err", err);
+  }
 });
